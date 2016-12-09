21f3c54e504f885e6195a8d5aaca9adc08a233e0
Samuel Christopher <thesamchris@gmail.com>
2016-12-09 22:09:40 +0800

    Fine tuned config

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,6 +3,9 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    'https://npmcdn.com/flickity@2/dist/flickity.pkgd.js',
+    'https://fonts.googleapis.com/css?family=Karla:400,700'
+
   ],
   navigateFallback: '/index.html'
 };
